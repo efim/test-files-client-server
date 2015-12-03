4e07703ce6a59b7742f5e3f9df24eb9b6088913c
efim <efim.wool@gmail.com>
2015-12-04 00:15:43 +0300

    unit tests stubs failing

--- src/test/java/com/excercise/filemanager/FileManagerTest.java
+++ src/test/java/com/excercise/filemanager/FileManagerTest.java
@@ -1,5 +1,36 @@
 package com.excercise.filemanager;
 
-public class FileManagerTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 
+import com.excercise.TestFilesClientServerApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(TestFilesClientServerApplication.class)
+public class FileManagerTest {
+	@Autowired
+	FileManager fileManager;
+
+	@Test
+	public void testAdd() {
+		throw new RuntimeException("this test is not implemented yet");
+	};
+	
+	@Test
+	public void testRemove() {
+		throw new RuntimeException("this test is not implemented yet");
+	};
+	
+	@Test
+	public void testRetrieve() {
+		throw new RuntimeException("this test is not implemented yet");
+	};
+	
+	@Test
+	public void testFind() {
+		throw new RuntimeException("this test is not implemented yet");
+	};
 }

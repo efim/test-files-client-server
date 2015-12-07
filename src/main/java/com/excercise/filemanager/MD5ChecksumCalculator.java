82f3fb5a50aecf1c1c0c65e106c3887eaab6bc24
efim <efim.wool@gmail.com>
2015-12-07 22:10:30 +0300

    file md5 checksum using apache commons

--- src/main/java/com/excercise/filemanager/MD5ChecksumCalculator.java
+++ src/main/java/com/excercise/filemanager/MD5ChecksumCalculator.java
@@ -1,16 +1,25 @@
 package com.excercise.filemanager;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 class MD5ChecksumCalculator implements UIDGenerator {
 
 	@Override
-	public String getUID(File file) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getUID(File file) {		
+        String checksum = null;
+        try {  
+            checksum = DigestUtils.md5Hex(new FileInputStream(file));
+        } catch (IOException ex) {
+        	//do nothing for now
+        	//TODO: insert logging
+        }
+        return checksum;
 	}
 
 }

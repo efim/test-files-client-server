75694ff2ff57d4cb54def1276d8de64c19646405
efim <efim.wool@gmail.com>
2015-12-09 01:24:20 +0300

    imports orginized

--- src/main/java/com/excercise/filemanager/MD5ChecksumUIDGenerator.java
+++ src/main/java/com/excercise/filemanager/MD5ChecksumUIDGenerator.java
@@ -1,10 +1,7 @@
 package com.excercise.filemanager;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Component;
--- src/main/java/com/excercise/filemanager/UIDGenerator.java
+++ src/main/java/com/excercise/filemanager/UIDGenerator.java
@@ -1,7 +1,5 @@
 package com.excercise.filemanager;
 
-import java.io.File;
-
 interface UIDGenerator {
 	public String getUID(byte[] fileData);
 }

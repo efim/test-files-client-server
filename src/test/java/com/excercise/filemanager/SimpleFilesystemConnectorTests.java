0868ced449024acce67559a9c52d6b6cdbc11802
efim <efim.wool@gmail.com>
2015-12-09 01:30:04 +0300

    Stubs for FilesystemConnectorTests. read, write, delete

---
+++ src/test/java/com/excercise/filemanager/SimpleFilesystemConnectorTests.java
@@ -0,0 +1,29 @@
+package com.excercise.filemanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.excercise.TestFilesClientServerApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(TestFilesClientServerApplication.class)
+public class SimpleFilesystemConnectorTests {
+
+	@Test
+	public void testWrite() {
+		throw new RuntimeException("Test is not implemented.");
+	};
+	
+	@Test
+	public void testRead() {
+		throw new RuntimeException("Test is not implemented.");
+	};
+	
+	@Test
+	public void testDelete() {
+		throw new RuntimeException("Test is not implemented.");
+	};
+	
+}

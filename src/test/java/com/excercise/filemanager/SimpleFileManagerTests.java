a00cb56795bf27868ee0b3e8eb3c2e6ad0092fd2
efim <efim.wool@gmail.com>
2015-12-09 01:22:13 +0300

    mock tests for SimpleFileManager

--- src/test/java/com/excercise/filemanager/SimpleFileManagerTests.java
+++ src/test/java/com/excercise/filemanager/SimpleFileManagerTests.java
@@ -1,11 +1,12 @@
 package com.excercise.filemanager;
 
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.excercise.TestFilesClientServerApplication;
 
@@ -13,32 +14,79 @@ import com.excercise.TestFilesClientServerApplication;
 @SpringApplicationConfiguration(TestFilesClientServerApplication.class)
 public class SimpleFileManagerTests {
 
-    @Before
-    public void setUp() {
-        //set up name repository mock
-    	//myServiceStub = new MyServiceStub();
-        
-    	//create my file manager
-    	//myLauncher = new MyLauncher(myServiceStub);
-    }
-
 	@Test
-	public void testAdd() {
-		throw new RuntimeException("this test is not implemented yet");
+	public void testAddNewFile() {
+		NameRepository mockNameRepository = Mockito.mock(NameRepository.class);
+		FilesystemConnector mockFilesystemConnector = Mockito.mock(FilesystemConnector.class);
+		UIDGenerator uidGenerator = new MD5ChecksumUIDGenerator();
+
+		byte[] data = javax.xml.bind.DatatypeConverter.parseHexBinary("hello world");
+
+		String expectedFileId = uidGenerator.getUID(data);		
+		
+		SimpleFileManager fileManager = new SimpleFileManager(mockNameRepository, uidGenerator, mockFilesystemConnector);
+		fileManager.add("file1.txt", data);
+		
+		Mockito.verify(mockNameRepository).add("file1.txt", expectedFileId);
+		Mockito.verify(mockFilesystemConnector).write(expectedFileId, data);		
 	};
 	
 	@Test
+	public void testAddDuplicate() {
+		NameRepository mockNameRepository = Mockito.mock(NameRepository.class);
+		FilesystemConnector mockFilesystemConnector = Mockito.mock(FilesystemConnector.class);
+		UIDGenerator uidGenerator = new MD5ChecksumUIDGenerator();
+
+		byte[] data = javax.xml.bind.DatatypeConverter.parseHexBinary("hello world");
+
+		String expectedFileId = uidGenerator.getUID(data);		
+		
+		SimpleFileManager fileManager = new SimpleFileManager(mockNameRepository, uidGenerator, mockFilesystemConnector);
+		fileManager.add("file1.txt", data);
+		fileManager.add("file2-duplicate.txt", data);
+		fileManager.add("another duplicate.txt", data);
+		
+		Mockito.verify(mockNameRepository).add("file2-duplicate.txt", expectedFileId);
+		Mockito.verify(mockNameRepository).add("another duplicate.txt", expectedFileId);
+		Mockito.verify(mockFilesystemConnector, Mockito.atMost(1)).write(expectedFileId, data);
+		
+	}
+	
+	@Test
 	public void testRemove() {
-		throw new RuntimeException("this test is not implemented yet");
+		NameRepository mockNameRepository = Mockito.mock(NameRepository.class);
+		FilesystemConnector mockFilesystemConnector = Mockito.mock(FilesystemConnector.class);
+		UIDGenerator uidGenerator = new MD5ChecksumUIDGenerator();
+		
+		SimpleFileManager fileManager = new SimpleFileManager(mockNameRepository, uidGenerator, mockFilesystemConnector);
+
+		fileManager.remove("1");
+		Mockito.verify(mockNameRepository).remove("1");
+		Mockito.verify(mockFilesystemConnector).delete("1");
+	
 	};
 	
 	@Test
 	public void testRetrieve() {
-		throw new RuntimeException("this test is not implemented yet");
+		NameRepository mockNameRepository = Mockito.mock(NameRepository.class);
+		FilesystemConnector mockFilesystemConnector = Mockito.mock(FilesystemConnector.class);
+		UIDGenerator uidGenerator = new MD5ChecksumUIDGenerator();
+		byte[] expectedData = javax.xml.bind.DatatypeConverter.parseHexBinary("hello world");
+		
+		Mockito.when(mockFilesystemConnector.read("1")).thenReturn(expectedData);		
+		
+		SimpleFileManager fileManager = new SimpleFileManager(mockNameRepository, uidGenerator, mockFilesystemConnector);
+		
+		assertEquals(expectedData, fileManager.retrieve("1"));
 	};
 	
 	@Test
 	public void testFind() {
-		throw new RuntimeException("this test is not implemented yet");
-	};
+		NameRepository mockNameRepository = Mockito.mock(NameRepository.class);
+
+		SimpleFileManager fileManager = new SimpleFileManager(mockNameRepository, null, null);
+		
+		fileManager.find("file1");
+		
+		Mockito.verify(mockNameRepository).find("file1");	};
 }

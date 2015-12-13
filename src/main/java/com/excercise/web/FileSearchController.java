4abac9ac1c08580c9c2aedb8747ca9ca6c302e7c
efim <efim.wool@gmail.com>
2015-12-13 19:33:57 +0300

    FileSearchController implementation and test to Standalone setup

---
+++ src/main/java/com/excercise/web/FileSearchController.java
@@ -0,0 +1,36 @@
+package com.excercise.web;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.excercise.filemanager.FileManager;
+
+@Controller
+@RequestMapping("/find")
+public class FileSearchController {
+	FileManager fileManager;
+	
+	@Autowired
+	public FileSearchController(FileManager fileManager) {
+		this.fileManager = fileManager;
+	}
+	
+	@RequestMapping(method=GET)
+	public String search(@RequestParam("fileName") String fileName, Model model) {
+		Map<String, String> searchResult = fileManager.find(fileName);
+		
+		model.addAttribute("resultList", searchResult);
+		
+		return "searchResults";
+		
+		
+	}
+	
+}
--- src/test/java/com/excercise/web/FileSearchControllerTests.java
+++ src/test/java/com/excercise/web/FileSearchControllerTests.java
@@ -3,39 +3,33 @@ package com.excercise.web;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.excercise.TestFilesClientServerApplication;
 import com.excercise.filemanager.FileManager;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(TestFilesClientServerApplication.class)
+
 public class FileSearchControllerTests {
-	private MockMvc mockMvc;
-	
-	@Autowired
-	FileManager fileManagerMock;
-	
+		
 	@Test
 	public void testFileSearchResults() throws Exception {
 		String testFileNamePart = "test";
+		FileManager fileManagerMock = Mockito.mock(FileManager.class);
 
 		Map<String, String> expectedMap = new HashMap<String, String>();
 		expectedMap.put("11test11.txt", "1");
 		expectedMap.put("22test222.pdf", "22");
 		
-		
 		Mockito.stub(fileManagerMock.find(testFileNamePart)).toReturn(expectedMap);
+		
+		FileSearchController controller = new FileSearchController(fileManagerMock);
+		MockMvc mockMvc = standaloneSetup(controller).build();
 		
 		mockMvc.perform(get("/find").param("fileName", testFileNamePart))
 			.andExpect(view().name("searchResults"))

0b534f5cc8c90f563e73d0b84d9e0f87755911f1
efim <efim.wool@gmail.com>
2015-12-13 17:06:02 +0300

    Test is remade in image of "Spring in Action book", added implementation

--- src/test/java/com/excercise/web/HomeControllerTest.java
+++ src/test/java/com/excercise/web/HomeControllerTest.java
@@ -2,23 +2,18 @@ package com.excercise.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.excercise.TestFilesClientServerApplication;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(TestFilesClientServerApplication.class)
 public class HomeControllerTest {
-	
-	private MockMvc mockMvc;
 	
 	@Test
 	public void testRenderHomePage() throws Exception {
+		HomeController controller = new HomeController();
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		
 		mockMvc.perform(get("/")).andExpect(view().name("homepage"));
 	}
 

2f0b42c9f5c400d625c352888174517e9ceb3323
efim <efim.wool@gmail.com>
2015-12-14 21:06:08 +0300

    dependency and bean for MaxUploadSize

--- src/main/java/com/excercise/TestFilesClientServerApplication.java
+++ src/main/java/com/excercise/TestFilesClientServerApplication.java
@@ -2,11 +2,20 @@ package com.excercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
 
 @SpringBootApplication
 public class TestFilesClientServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestFilesClientServerApplication.class, args);
+    }
+    
+    @Bean
+    public MultipartResolver multipartResolver() {
+        org.springframework.web.multipart.commons.CommonsMultipartResolver multipartResolver = new org.springframework.web.multipart.commons.CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(52428800);
+        return multipartResolver;
     }
 }
